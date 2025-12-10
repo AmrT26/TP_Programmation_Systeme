0400bea844651f22db262d71690d87fbf35089e1
Amr <amrtaouis@gmail.com>
2025-12-10 11:23:46 -0500

    Lecture de la commande saisie, Execution d'une commande simple (sans argument), Retour au prompt enseash % et attente de la commande suivante

---
+++ enseash2.c
@@ -0,0 +1,52 @@
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <stdlib.h>
+
+#define WELCOME "Welcome in the shell ENSEA. \nTo exit, type 'exit'.\n"
+#define PROMPT  "enseash % "
+#define BUFFER_SIZE 256
+
+int main(void){
+	char buffer[BUFFER_SIZE];
+	ssize_t bytes_read;
+
+        write(STDOUT_FILENO, WELCOME, strlen(WELCOME));
+
+         while (1) {
+        write(STDOUT_FILENO, PROMPT, strlen(PROMPT));
+
+        bytes_read = read(STDIN_FILENO, buffer, BUFFER_SIZE);
+        if (bytes_read <= 0) {
+            continue;
+        }
+
+        if (buffer[bytes_read - 1] == '\n') {
+            buffer[bytes_read - 1] = '\0';
+        }
+
+        if (strncmp(buffer, "exit", 4) == 0) {
+            return 0;
+        }
+
+        pid_t pid = fork();
+
+        if (pid == 0) {
+            char *args[] = { buffer, NULL };
+            execvp(buffer, args);
+
+            write(STDOUT_FILENO, "Command not found\n", strlen("Command not found\n"));
+            exit(1);
+        }
+        else if (pid > 0) {
+            wait(NULL);
+        }
+        else {
+            write(STDOUT_FILENO, "Fork error\n", strlen("Fork error\n"));
+        }
+    }
+
+    return 0;
+}
+
